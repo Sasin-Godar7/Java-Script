85973fb03adef09c2008177eea311576d6999b7d
Sasin Godar <sago_bca2080@lict.edu.np>
2025-06-21 21:23:16 +0545

    update

--- String.js
+++ String.js
@@ -1,34 +1,34 @@
- //collection of character is string
+//collection of character is string
 
-        let name = "sasin godar";
-        console.log(name);
-        console.log(name[0]);
-        console.log(name[1]);
+let name = "sasin godar";
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
 
-        let friend = 'aakash';
-        console.log(friend.length);
+let friend = 'aakash';
+console.log(friend.length);
 
-        //Template Literals
+//Template Literals
 
-        let boy1 = "Sasin";
-        let boy2 = "Prabin";
+let boy1 = "Sasin";
+let boy2 = "Prabin";
 
-        // let sentence = `boy1 is the friend of boy2`;    this print the as it is
-        let sentence = `${boy1} is the friend of ${boy2}`;   // this will put the value in the variables
-        console.log(sentence);
+// let sentence = `boy1 is the friend of boy2`;    this print the as it is
+let sentence = `${boy1} is the friend of ${boy2}`;   // this will put the value in the variables
+console.log(sentence);
 
-        //Escape Sequence Character
+//Escape Sequence Character
 
-        let country = 'Ameri\'ca';
-          console.log(country);
-          console.log(country.length);  // this will increase 1 character
+let country = 'Ameri\'ca';
+console.log(country);
+console.log(country.length);  // this will increase 1 character
 
-          let fruit = "Banana \n apple";
-          console.log(fruit);
+let fruit = "Banana \n apple";
+console.log(fruit);
 
-          let str = "sasin ";
-          let strr = "godar";
+let str = "sasin ";
+let strr = "godar";
 
-          let jodni = `${strr} is the surname of ${str}` ;
-          alert(jodni);
-          alert(strr.length);
+let jodni = `${strr} is the surname of ${str}`;
+alert(jodni);
+alert(strr.length);
